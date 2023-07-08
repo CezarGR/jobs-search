24cbe691e35a1c0fc67ced3cdf58a16ac439f2dc
CezarGR <100092984+CesarGRodrigues@users.noreply.github.com>
2023-07-07 22:58:24 -0300

    feat: creation new HeaderComponent

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+import HeaderComponent from '@/components/HeaderComponent.vue';
 </script>
 
 <template>
   <header>
     <div class="wrapper">
       <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <header-component />
+        <!-- <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink> -->
       </nav>
     </div>
   </header>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,35 @@
+<template>
+  <nav class="HeaderComponent">
+    <slot>
+        <span></span>
+    </slot>
+    <div class="HeaderComponent__navigation">
+        <a href="">conta</a>
+        <a href="">home</a>
+        <a href="">empresa</a>
+    </div>
+    <span class="material-symbols-outlined">favorite</span>
+  </nav>
+</template>
+
+<script>
+export default {
+    name: 'HeaderComponent',
+    data: () => ({
+
+    })
+}
+</script>
+
+<style scoped lang="scss">
+.HeaderComponent {
+    display: flex;
+    justify-content: space-between;
+
+    &__navigation {
+        a {
+            margin: 0 10px;
+        }
+    }
+}
+</style>
